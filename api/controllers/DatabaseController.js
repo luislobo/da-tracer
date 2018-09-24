dbd820da137ac0ae368130cd6728cb6216cc803e
Luis Lobo Borobia <luislobo@gmail.com>
2018-09-23 19:40:17 -0500

    Reformatted code, made connection page handle cluster, user and password

--- api/controllers/DatabaseController.js
+++ api/controllers/DatabaseController.js
@@ -1,14 +1,26 @@
-
-
 module.exports = {
-  showConnectPage(req, res) {
+  showConnectPage (req, res) {
     res.view('connect', {
       formEndpoint: '/connect'
     })
   },
-  connect(req, res) {
-    if (req.body.uri && req.body.databaseName) {
-      return MongodbService.connect(req.body.uri, req.body.databaseName)
+  connect (req, res) {
+    const body = req.body
+
+    // default to body
+    let uri = body.uri
+
+    // if database set
+    if (body.databaseName) {
+
+      // and if all the other settings are set
+      if (body.clusterName && body.user && body.password) {
+        // recreate uri
+        uri = `mongodb+srv://${encodeURIComponent(body.user)}:${encodeURIComponent(body.password)}@${body.clusterName}/${body.databaseName}?retryWrites=true`
+      }
+
+      console.log(uri)
+      return MongodbService.connect(uri, body.databaseName)
         .then(() => {
           return MongodbService.isReplicaSet()
             .then((replicaSetStatus) => {
@@ -18,33 +30,36 @@ module.exports = {
         })
         .catch(res.serverError)
     }
+
     return res.badRequest('Missing connection options')
   },
-  close(req, res) {
+  close (req, res) {
     if (!req.isSocket) {
       res.badRequest('Socket request required')
     }
-    sails.sockets.leave(req, 'change-stream');
+    sails.sockets.leave(req, 'change-stream')
     MongodbService.removeAllListeners()
     MongodbService.unsubscribeFromAll()
       .then(() => MongodbService.close())
       .then(() => res.ok())
       .catch(res.serverError)
-  },
-  showStreamPage(req, res) {
+  }
+  ,
+  showStreamPage (req, res) {
     if (MongodbService.isConnected()) {
       return res.view('stream')
     }
     return res.redirect('/connect')
-  },
-  startStream(req, res) {
+  }
+  ,
+  startStream (req, res) {
     if (!MongodbService.isConnected()) {
       res.serverError('Database not connected')
     }
     if (!req.isSocket) {
       res.badRequest('Socket request required')
     }
-    sails.sockets.join(req, 'change-stream');
+    sails.sockets.join(req, 'change-stream')
     MongodbService.on('change', (data) => {
 
       sails.log.verbose('received new change', data)
@@ -54,14 +69,14 @@ module.exports = {
       simpleData._id = data.documentKey._id.toString()
       simpleData.db = data.ns.db
       simpleData.collection = data.ns.coll
-      if(simpleData.operation === 'insert') {
+      if (simpleData.operation === 'insert') {
         simpleData.document = JSON.stringify(data.fullDocument, null, 2)
-      } else if(simpleData.operation === 'update') {
+      } else if (simpleData.operation === 'update') {
         simpleData.document = data.updateDescription
       }
       sails.log.verbose('broadcasting', simpleData)
       sails.log.info('new change', simpleData._id)
-      sails.sockets.broadcast('change-stream', 'message', simpleData);
+      sails.sockets.broadcast('change-stream', 'message', simpleData)
     })
     MongodbService.subscribeToAll()
       .then(() => {
